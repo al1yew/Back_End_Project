883b28c844f054d6f2494c5f64044d722bb83a99
al1yew <vasifja@code.edu.az>
2022-07-07 16:50:24 +0400

    First Commit - Some changes in Startup

--- Back_End_Project/Startup.cs
+++ Back_End_Project/Startup.cs
@@ -12,10 +12,9 @@ namespace Back_End_Project
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
